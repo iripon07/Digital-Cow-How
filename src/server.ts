5bcb5f34a35a0fc73a8317449d15887b37b8bb46
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-02-06 13:44:32 +0600

    installed husky eslin staged

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
-const port = 3000;
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log(`🍔 Database is connected successfully!`);
-    app.listen(port, () => {
-      console.log(`Application listening on port ${config.port}`);
-    });
+    await mongoose.connect(config.database_url as string)
+    console.log(`🍔 Database is connected successfully!`)
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log(`Failed to connect database`, error);
+    console.log(`Failed to connect database`, error)
   }
 }
 
-main();
+main()
