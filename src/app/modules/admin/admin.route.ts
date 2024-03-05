73e78c7be6750850a951987b6aca7df7d93b581b
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-03-05 15:29:29 +0600

    admin created successfully

--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { AdminControllers } from './admin.controller';
 import { AdminValidations } from './admin.validation';
@@ -7,6 +9,7 @@ const router = express.Router();
 
 router.post(
   '/create-admin',
+  auth(ENUM_USER_ROLE.ADMIN),
   validateRequest(AdminValidations.createAdminZodSchema),
   AdminControllers.createAdmin,
 );
--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -3,15 +3,15 @@ import ApiError from '../../../error/ApiError';
 import { IUser } from '../user/user.interface';
 import { User } from '../user/user.model';
 
-const createAdmin = async (admin: IUser): Promise<IUser | null> => {
+const createAdmin = async (payload: IUser): Promise<IUser | null> => {
   const checkPhoneNumber = await User.findOne({
-    phoneNumber: admin.phoneNumber,
+    phoneNumber: payload.phoneNumber,
   });
   if (checkPhoneNumber) {
     throw new ApiError(httpStatus.CONFLICT, `Already use this number`);
   }
-  admin.role = 'admin';
-  const createdAdmin = await User.create(admin);
+  payload.role = 'admin';
+  const createdAdmin = await User.create(payload);
   const result = await User.findById(createdAdmin._id);
   if (!createdAdmin) {
     throw new ApiError(httpStatus.BAD_REQUEST, `Failed to create admin`);
